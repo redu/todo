532e5b21dc53b038d864ebf191315ff1fa675d6e
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-03-28 14:22:08 -0300

    Adding cient-side behavior

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,2 +1,33 @@
 jQuery(function(){
+    
+    // Setting up
+    $("#todo").after("<li id=\"buffer\" class=\"clearfix\"> <form action=\"#\" class=\"edit\"> <fieldset> <input type=\"text\" value=\"Your TODO here.\" class=\"content\" /> <button>Update</button> </fieldset> </form> <form action=\"#\" class=\"remove\"> <fieldset> <input type=\"hidden\" /> <button>Remove</button> </fieldset> </form> </li>");
+    $("#buffer").hide();
+
+    $(".edit input[type=text]").hide()
+    $(".edit input[type=text]").each(function(){
+        var value = $(this).val();
+        $(this).after("<span class=\"item\">" + value + "</span>");
+    });
+
+    $('.item').live("dblclick", function(){
+        $(this).hide();
+        $(this).prev(":first").show();
+    });
+
+    $(".edit button").live("click", function(){
+        //console.log($(this).prev());
+        $(this).prevAll("input:first").toggle();
+        $(this).prev().toggle();
+    });
+
+    $(".remove button").live("click", function(){
+        $(this).parents("li:first").remove();
+    });
+
+    $("#new button").click(function(){
+        var value = $(".edit input[type=text]").val();
+        var html = $("#buffer").clone(true).show();
+        $("#todo li:last").after(html);
+    });
 });
